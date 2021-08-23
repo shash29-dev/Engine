693ff9d93af6955b00b0381f38f237d93965c038
Shashikant Verma <shashikantverma408@gmail.com>
2021-08-23 23:50:20 +0530

    Logging works!!
    Logging works Now

--- Alpha/src/Alpha/EntryPoint.h
+++ Alpha/src/Alpha/EntryPoint.h
@@ -5,6 +5,8 @@ extern Alpha::Application *Alpha::CreateApplication();
 int main(int argc, const char **argv)
 {
     Alpha::Log::Init();
+    Alpha::Log::GetCoreLogger()->warn("Initialized Core Log");
+    Alpha::Log::GetClientLogger()->info("Initialized Client log");
     auto app = Alpha::CreateApplication();
     app->Run();
     delete app;
--- Alpha/src/Alpha/Log.cpp
+++ Alpha/src/Alpha/Log.cpp
@@ -1,5 +1,6 @@
 #include "Log.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
+#include <iostream>
 namespace Alpha
 {
     std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
@@ -13,10 +14,19 @@ namespace Alpha
 
     void Log::Init()
     {
-        spdlog::set_pattern("%^[%T] %n: %v%$");
-        s_CoreLogger = spdlog::stdout_color_mt("Alpha");
-        s_CoreLogger->set_level(spdlog::level::trace);
-        s_ClientLogger = spdlog::stdout_color_mt("Alpha");
-        s_ClientLogger->set_level(spdlog::level::trace);
+        try
+        {
+            spdlog::set_pattern("%^[%T] %n: %v%$");
+            s_CoreLogger = spdlog::stdout_color_mt("Alpha");
+            s_CoreLogger->set_level(spdlog::level::trace);
+            s_ClientLogger = spdlog::stdout_color_mt("App");
+            s_ClientLogger->set_level(spdlog::level::trace);
+            spdlog::error("WELCOME?");
+        }
+        catch (const std::exception &e)
+        {
+            spdlog::info("Welcome to spdlog!");
+            std::cout << e.what() << "\n";
+        }
     }
 }
